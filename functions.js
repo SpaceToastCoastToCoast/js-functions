677da4df25f7fe8f117dab3db4251a0eb3c5e9ac
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-06 17:59:15 -1000

    passed tenth test: isGreaterThan returns whether a is greater than b

--- functions.js
+++ functions.js
@@ -113,6 +113,9 @@ function calculate(operation, x, y) {
  * @return {boolean} `a` is larger than `b`
  */
 
+function isGreaterThan(a, b) {
+  return a > b;
+}
 
 /**
  * Returns true if `a` is less than `b`.
